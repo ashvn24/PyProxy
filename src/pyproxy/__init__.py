305f221cfbbdee1a1aaf538e22c9cbafbebb7cc4
ashvn24 <ashwinvk77@gmail.com>
2026-08-02 21:30:16 +0530

    release

--- src/pyproxy/__init__.py
+++ src/pyproxy/__init__.py
@@ -29,7 +29,7 @@ except PackageNotFoundError:  # pragma: no cover
     try:
         __version__ = version("pyproxy")
     except PackageNotFoundError:
-        __version__ = "0.0.0-dev"
+        __version__ = "0.2.0"
 
 __all__: list[str] = [
     "Proxy",
